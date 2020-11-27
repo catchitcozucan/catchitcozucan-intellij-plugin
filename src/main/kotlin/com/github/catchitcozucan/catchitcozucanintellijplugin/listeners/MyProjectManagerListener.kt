e4987be7d94a6ff3b81955179a7317bca9f3a5b4
GitHub Action <action@github.com>
2020-11-27 21:33:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/catchitcozucan/catchitcozucanintellijplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/catchitcozucan/catchitcozucanintellijplugin/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.catchitcozucan.catchitcozucanintellijplugin.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.catchitcozucan.catchitcozucanintellijplugin.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/catchitcozucan/catchitcozucanintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.catchitcozucan.catchitcozucanintellijplugin.services
+
+import com.github.catchitcozucan.catchitcozucanintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
